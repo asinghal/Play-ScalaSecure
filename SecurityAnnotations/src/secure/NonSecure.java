fa87d502ac480e975786a1893f6e9d668d078e55
Aishwarya Singhal <asinghal79@gmail.com>
2011-08-18 00:04:33 +0530

    first commit

---
+++ SecurityAnnotations/src/secure/NonSecure.java
@@ -0,0 +1,12 @@
+package secure;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NonSecure {
+
+}
